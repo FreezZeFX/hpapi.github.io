fa49145150447fd80dd2a5ba8c39048ba35d007a
Fábio <fabiopinto.epbjc@gmail.com>
2022-08-09 00:37:03 +0100

    3 Commit: get students data + images

--- hpapi/src/Components/hp-apiget.js
+++ hpapi/src/Components/hp-apiget.js
@@ -5,12 +5,12 @@ import './hp-apiget.css';
 const Hpapiget = () => {
 
     const [characters, setCharacters] = useState([]);
-
+    const [images, setImages] = useState([]);
     useEffect(() => {
         
     const getCharacters = async () => {
-        const url = `http://hp-api.herokuapp.com/api/characters`;
-
+        const url = `http://hp-api.herokuapp.com/api/characters/students`;
+        
         try {
             const response = await axios.get(url);
             console.log(response.data);
@@ -27,15 +27,16 @@ const Hpapiget = () => {
 
     return ( 
             <div className='characterContainer'>
-            {characters.map((character, Index) => (
+            {characters.map ((character, Index) => (
                 <div key={Index}> 
+                    <img className='imageData' src={character.image}></img>
                     <ul className='characterData'>{[
-                        character.name,"" ,character.alternate_names, character.species, character.gender,
-                        character.house, character.dateOfBirth, character.yearOfBirth,
-                        character.wizard, character.ancestry, character.eyeColour,
-                        character.hairColour, character.wand.wood, character.wand.core, character.wand.length,
-                        character.patronus, character.hogwartsStudent,
-                        character.howartsStaff, character.actor, character.alternate_actors, character.alive
+                        character.name, <br/>, character.alternate_names, <br/>, character.species, <br/>, character.gender,
+                        character.house, <br/>, character.dateOfBirth, <br/>, character.yearOfBirth, <br/>,
+                        character.wizard, <br/>, character.ancestry, <br/>, character.eyeColour, <br/>,
+                        character.hairColour, <br/>, character.wand.wood, <br/>, character.wand.core, <br/>, character.wand.length, <br/>,
+                        character.patronus, <br/>, character.hogwartsStudent, <br/>,
+                        character.howartsStaff, <br/>, character.actor, <br/>, character.alternate_actors, <br/>, character.alive
                     ]}</ul>
                 </div>
             ))}
